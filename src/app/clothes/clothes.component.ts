9d4f288248875522135ebeb98b5088e8598f997d
ZGV9 <francocuelloo@gmail.com>
2024-03-26 11:00:35 -0300

    Model

--- src/app/clothes/clothes.component.ts
+++ src/app/clothes/clothes.component.ts
@@ -15,12 +15,12 @@ export class ClothesComponent {
   clothesList: Clothes[] = [];
 
   constructor() {
-    this.clothesList.push(new Clothes(1, "GloryBoys", "MarcaX", "M", 20, true, "assets/imgClothes/gloryboys-0602-1-92500820b5d79ba7fc17000782192917-640-0.webp"));
-    this.clothesList.push(new Clothes(2, "GloryBoys", "MarcaY", "L", 30, false, "assets/imgClothes/gloryboys-0609-d8d73545c95b57353417000807757037-640-0.webp"));
-    this.clothesList.push(new Clothes(2, "CowBoys", "MarcaY", "L", 22, false, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-131-ae3f4c47ea21c2a1b116910013909312-640-0.webp"));
-    this.clothesList.push(new Clothes(2, "CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-31-cdf1155bfb754c29d016909991802075-640-0.webp"));
-    this.clothesList.push(new Clothes(2, "CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-301-d57f744b0c28183d8a16909996784269-640-0.webp"));
-    this.clothesList.push(new Clothes(2, "CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-281-3233f6c7c2a5532a8316910003221955-640-0.webp"));
+    this.clothesList.push(new Clothes(1, "Remera-GloryBoys", "MarcaX", "M", 20, true, "assets/imgClothes/gloryboys-0602-1-92500820b5d79ba7fc17000782192917-640-0.webp"));
+    this.clothesList.push(new Clothes(2, "Bermuda-GloryBoys", "MarcaY", "L", 30, false, "assets/imgClothes/gloryboys-0609-d8d73545c95b57353417000807757037-640-0.webp"));
+    this.clothesList.push(new Clothes(3, "Buzo-CowBoys", "MarcaY", "L", 22, false, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-131-ae3f4c47ea21c2a1b116910013909312-640-0.webp"));
+    this.clothesList.push(new Clothes(2, "Remera-CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-31-cdf1155bfb754c29d016909991802075-640-0.webp"));
+    this.clothesList.push(new Clothes(2, "Pantalon-CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-301-d57f744b0c28183d8a16909996784269-640-0.webp"));
+    this.clothesList.push(new Clothes(2, "Remera-CowBoys", "MarcaY", "XL", 24, true, "assets/imgClothes/productos_mesa-de-trabajo-1-copia-281-3233f6c7c2a5532a8316910003221955-640-0.webp"));
   
   }
 }
@@ -28,7 +28,7 @@ export class ClothesComponent {
 
 Swal.fire({
   title: "WELCOME TO THE ONLINE SHOP IMS24",
-  background: "#d65d3e",
+  background: "#fff",
   color: "#000",
   showConfirmButton:false,
   iconColor: '#000',
