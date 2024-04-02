a3cc98b48086b7564ff54fe4b3554d3559e578c1
ZGV9 <francocuelloo@gmail.com>
2024-04-02 18:50:29 -0300

    Forms

--- src/app/components/reactive-forms/reactive-forms.component.ts
+++ src/app/components/reactive-forms/reactive-forms.component.ts
@@ -15,21 +15,15 @@ import Swal from 'sweetalert2';
 export class ReactiveFormsComponent {
 
   userForm = this.formBuilder.group({
-    // name: this.formBuilder.control(''),
     lastName: this.formBuilder.control(''),
     email: this.formBuilder.control('', [
-      // Validators.email,
       Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}'),
       Validators.required,
     ]),
     contrasena: ['', Validators.required],
   });
 
-  // SIN FORM BUILDER
-  // userForm = new FormGroup({
-  //   name: new FormControl(''),
-  //   email: new FormControl()
-  // })
+
 
   get emailControl() {
     return this.userForm.get('email');
